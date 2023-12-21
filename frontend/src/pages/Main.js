5cc919fdb1814da2b2207b264316cf5f35ecacbe
toto9 <aaba03@gmail.com>
2023-12-21 17:10:44 -0300

    input de filtro de restaurantes inplementado

--- frontend/src/pages/Main.js
+++ frontend/src/pages/Main.js
@@ -2,9 +2,11 @@ import apiClient from "../api/http-common";
 import { Header } from "../components/header";
 import { HorizontalCard } from "../components/restaurantCard";
 import React, { useState, useEffect } from "react";
-
+import { Input } from "@material-tailwind/react";
+import { motion } from "framer-motion";
 
 export const Main = () => {
+  const [query, setQuery] = useState("");
   const [restaurants, setRestaurants] = useState([]);
 
   const receiveRestaurants = async () => {
@@ -14,9 +16,12 @@ export const Main = () => {
         "Content-Type": "application/json",
         Authorization: `Bearer ${actualToken}`,
       };
-      const response = await apiClient.get("/restaurants", { headers });
+      const response = await apiClient.get(`/restaurants?query=${query}`, {
+        headers,
+      });
       console.log(response.data);
       setRestaurants(response.data);
+    
     } catch (error) {
       console.log(error);
     }
@@ -24,16 +29,42 @@ export const Main = () => {
 
   useEffect(() => {
     receiveRestaurants();
-  }, []);
+  }, [query]);
 
   return (
-    <div className="pt-24 bg-gray-200 w-full flex flex-col justify-center items-center h-full ">
+    <div className="pt-28 bg-gray-200 w-full min-h-screen flex flex-col justify-center items-center">
       <Header />
-      {restaurants.map((restaurant, index) => (
-        <div className="p-6 items-center justify-center" key={index}>
-          <HorizontalCard restaurant={restaurant} />
-        </div>
-      ))}
+
+      <div className="max-w-[32rem] shadow-lg box-border flex items-center justify-center gap-4 bg-white backdrop-blur-md rounded-md w-full px-4 py-4">
+        <input
+          onChange={(e) => setQuery(e.target.value)}
+          className="w-full h-full bg-transparent text-gray-800 text-lg font-semibold border-none outline-none pl-3 pr-10"
+        />
+        <motion.button>
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            className="h-6 w-6 absolute right-5 top-1/2 transform -translate-y-1/2 text-gray-800"
+            fill="none"
+            viewBox="0 0 24 24"
+            stroke="currentColor"
+          >
+            <path
+              strokeLinecap="round"
+              strokeLinejoin="round"
+              strokeWidth={2}
+              d="M21 21l-6-6m2-5a7 7 0 11-14 0 7 7 0 0114 0z"
+            />
+          </svg>
+        </motion.button>
+      </div>
+
+      <div className="flex-grow">
+        {restaurants.map((restaurant, index) => (
+          <div className="p-6 justify-center" key={index}>
+            <HorizontalCard restaurant={restaurant} />
+          </div>
+        ))}
+      </div>
     </div>
   );
 };
--- frontend/src/pages/Restaurant.js
+++ frontend/src/pages/Restaurant.js
@@ -3,10 +3,10 @@ import { BookingCard } from "../components/teste";
 
 export const Restaurant = () => {
   return (
-    <div className="pt-28 bg-gray-200 w-full flex flex-col justify-center items-center h-screen">
+    <div className="pt-28 bg-gray-200 w-full flex justify-center items-center h-screen">
         <Header />
-        <BookingCard/>
-      <h1>Restaurant</h1>
+        <div> <BookingCard/></div>
+        <div> <BookingCard/></div>
     </div>
   );
 };
