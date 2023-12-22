84434a8de944dceed1619863c5b93d297245ac11
toto9 <aaba03@gmail.com>
2023-12-22 12:44:11 -0300

    feat carousel of itens in restaurant pag

--- frontend/src/components/restaurantCardExtended.js
+++ frontend/src/components/restaurantCardExtended.js
@@ -16,16 +16,16 @@ export function RestaurantCardExtended({ restaurant }) {
   };
 
   return (
-    <Card className="w-full max-w-[72rem] flex-row rounded-lg">
+    <Card className="w-full max-w-[60rem] flex-row rounded-lg">
       <CardHeader
         shadow={false}
         floated={false}
-        className="m-0 w-2/5 shrink-0 rounded-r-none"
+        className="m-0 w-2/5 shrink-0"
       >
         <img
           src="https://images.unsplash.com/photo-1522202176988-66273c2fd55f?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1471&q=80"
           alt="card-image"
-          className="h-full w-full object-cover rounded-lg"
+          className="h-full w-full object-cover"
         />
       </CardHeader>
       <CardBody>
--- frontend/src/pages/Restaurant.js
+++ frontend/src/pages/Restaurant.js
@@ -3,13 +3,28 @@ import { BookingCard } from "../components/teste";
 import { RestaurantCardExtended } from "../components/restaurantCardExtended";
 import { useRestaurant } from "../context/RestaurantContext";
 import { Typography } from "@material-tailwind/react";
-import { Carousel } from 'react-responsive-carousel';
-import "react-responsive-carousel/lib/styles/carousel.min.css"; // Importa os estilos do carrossel
+import Carousel from "react-multi-carousel";
+import "react-multi-carousel/lib/styles.css";
 
 // https://reactjsexample.com/a-lightweight-production-ready-carousel-for-react/
 
 export const Restaurant = () => {
   const { selectedRestaurant } = useRestaurant();
+
+  const responsive = {
+    desktop: {
+      breakpoint: { max: 3000, min: 1024 },
+      items: 3,
+    },
+    tablet: {
+      breakpoint: { max: 1024, min: 640 },
+      items: 2,
+    },
+    mobile: {
+      breakpoint: { max: 640, min: 0 },
+      items: 1,
+    },
+  };
 
   return (
     <div className="pt-28 bg-gray-200 w-full min-h-screen items-center">
@@ -17,18 +32,57 @@ export const Restaurant = () => {
       <div className="flex justify-center">
         <RestaurantCardExtended restaurant={selectedRestaurant} />{" "}
       </div>
-      <div className=" flex justify-start ml-96 mt-12 mb-12">
+      <div className=" flex justify-start ml-96 mt-6 mb-6">
         <Typography variant="h2" color="gray">
           Pratos
         </Typography>
       </div>
 
-      <div className=" flex justify-center">
-        <Carousel>
-          <div><BookingCard /></div>
-          <div><BookingCard /></div>
-          <div><BookingCard /></div>
-          {/* Adicione quantos BookingCard você quiser */}
+      <div className="flex justify-center mx-60">
+        <Carousel
+          infinite={true}
+          containerClass="w-full"
+          responsive={responsive}
+        >
+          <div className="flex justify-center">
+            <BookingCard />
+          </div>
+          <div className="flex justify-center">
+            <BookingCard />
+          </div>
+          <div className="flex justify-center">
+            <BookingCard />
+          </div>
+          <div className="flex justify-center">
+            <BookingCard />
+          </div>
+        </Carousel>
+      </div>
+
+      <div className=" flex justify-start ml-96 mt-6 mb-6">
+        <Typography variant="h2" color="gray">
+          Bebidas
+        </Typography>
+      </div>
+
+      <div className="flex justify-center mx-60">
+        <Carousel
+          infinite={true}
+          containerClass="w-full"
+          responsive={responsive}
+        >
+          <div className="flex justify-center">
+            <BookingCard />
+          </div>
+          <div className="flex justify-center">
+            <BookingCard />
+          </div>
+          <div className="flex justify-center">
+            <BookingCard />
+          </div>
+          <div className="flex justify-center">
+            <BookingCard />
+          </div>
         </Carousel>
       </div>
     </div>
